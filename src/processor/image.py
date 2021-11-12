9b586a9cd89f6c6e983df02be06da98aec71c8f2
dparikesit <dsparikesit@gmail.com>
2021-11-12 10:36:05 +0700

    simultaneous power iteration

--- src/processor/image.py
+++ src/processor/image.py
@@ -1,21 +1,54 @@
 import cv2 as cv
 import sys
+import numpy as np
+
+
+def simultaneous_power_iteration(A, k):
+  n, m = A.shape
+  Q = np.random.rand(n, k)
+  Q, _ = np.linalg.qr(Q)
+  Q_prev = Q
+  for i in range(1000):
+      Z = A.dot(Q)
+      Q, R = np.linalg.qr(Z)
+      # can use other stopping criteria as well
+      err = ((Q - Q_prev) ** 2).sum()
+      Q_prev = Q
+      if err < 1e-3:
+          break
+  return np.diag(R), Q
 
 def compress(filePath):
-  #Import Image
-  img = cv.imread(filePath)
-  BGRArray = cv.split(img)
-  print(img.shape)
+  # #Import Image
+  # img = cv.imread(filePath)
+  # BGRArray = cv.split(img)
+  # print(img.shape)
 
-  #Compressing Image
-  for color in BGRArray:
-    color = (list(color))
+  # #Compressing Image
+  # for color in BGRArray:
+  #   color = (list(color))
     
 
 
-  #Export File
-  img_bgr = cv.merge(BGRArray)
-  cv.imwrite('Test2.jpg',img_bgr)
+  # #Export File
+  # img_bgr = cv.merge(BGRArray)
+  # cv.imwrite('Test2.jpg',img_bgr)
+
+  A = np.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+  rows, cols = A.shape
+  mode = False
+  if rows < cols:
+    mode = True  # True jika return U, False return V
+
+  eigen, arr = simultaneous_power_iteration(A, min(A.shape))
+  print("Eigen")
+  print(eigen)
+
+  if mode==True:
+    print("U")
+  else:
+    print("V")
+  print(arr)
 
 
 if __name__=="__main__":
